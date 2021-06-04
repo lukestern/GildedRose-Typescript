4bdad60b2581fdb2ce6b8a970719a2e494092aa1
Luke Stern <luke.stern@softwire.com>
2021-06-04 12:00:58 +0100

    increace quailty function, parametrize item

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -12,66 +12,75 @@ export class Item {
 
 export class GildedRose {
     items: Array<Item>;
+    maxQuality: number;
+    minQuality: number;
 
     constructor(items = [] as Array<Item>) {
         this.items = items;
+        this.maxQuality = 50;
+        this.minQuality = 0;
     }
 
     updateQuality() {
         let sulfuras = 'Sulfuras, Hand of Ragnaros';
         let brie = 'Aged Brie';
         let backstagePasses = 'Backstage passes to a TAFKAL80ETC concert';
-        let minQuality = 0;
-        let maxQuality = 50;
         let tenDaysTillConcert = 10;
         let fiveDaysTillConcert = 5;
-        
+
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != brie && this.items[i].name != backstagePasses) {
-                if (this.items[i].quality > minQuality) {
-                    if (this.items[i].name != sulfuras) {
-                        this.items[i].quality--;
+            const item = this.items[i];
+
+            if (item.name != brie && item.name != backstagePasses) {
+                if (item.quality > this.minQuality) {
+                    if (item.name != sulfuras) {
+                        item.quality--;
                     }
                 }
             } else {
-                if (this.items[i].quality < maxQuality) {
-                    this.items[i].quality++;
-                    if (this.items[i].name == backstagePasses) {
-                        if (this.items[i].sellIn <= tenDaysTillConcert) {
-                            if (this.items[i].quality < maxQuality) {
-                                this.items[i].quality++;
-                            }
+                if (item.quality < this.maxQuality) {
+                    item.quality++;
+                    if (item.name == backstagePasses) {
+
+                        if (item.sellIn <= tenDaysTillConcert) {
+                            this.increaseQualityUntilMaximum(item);
                         }
-                        if (this.items[i].sellIn <= fiveDaysTillConcert) {
-                            if (this.items[i].quality < maxQuality) {
-                                this.items[i].quality++;
-                            }
+
+                        if (item.sellIn <= fiveDaysTillConcert) {
+                            this.increaseQualityUntilMaximum(item);
                         }
                     }
                 }
             }
-            if (this.items[i].name != sulfuras) {
-                this.items[i].sellIn--
+
+            if (item.name != sulfuras) {
+                item.sellIn--
             }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != brie) {
-                    if (this.items[i].name != backstagePasses) {
-                        if (this.items[i].quality > minQuality) {
-                            if (this.items[i].name != sulfuras) {
-                                this.items[i].quality--;
+
+            if (item.sellIn < 0) {
+                if (item.name != brie) {
+                    if (item.name != backstagePasses) {
+                        if (item.quality > this.minQuality) {
+                            if (item.name != sulfuras) {
+                                item.quality--;
                             }
                         }
                     } else {
-                        this.items[i].quality = minQuality;
+                        item.quality = this.minQuality;
                     }
                 } else {
-                    if (this.items[i].quality < maxQuality) {
-                        this.items[i].quality++;
-                    }
+                    this.increaseQualityUntilMaximum(item);
                 }
             }
         }
 
         return this.items;
     }
+
+    increaseQualityUntilMaximum(item) {
+        if (item.quality < this.maxQuality) {
+            item.quality++;
+        }
+    }
+
 }
