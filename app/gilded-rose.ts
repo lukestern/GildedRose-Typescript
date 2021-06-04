70e0acff1082045199215b652cbf8dbc543acb1b
Luke Stern <luke.stern@softwire.com>
2021-06-04 11:41:16 +0100

    Parameterize numbers

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,26 +21,30 @@ export class GildedRose {
         let sulfuras = 'Sulfuras, Hand of Ragnaros';
         let brie = 'Aged Brie';
         let backstagePasses = 'Backstage passes to a TAFKAL80ETC concert';
+        let minQuality = 0;
+        let maxQuality = 50;
+        let tenDaysTillConcert = 10;
+        let fiveDaysTillConcert = 5;
         
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != brie && this.items[i].name != backstagePasses) {
-                if (this.items[i].quality > 0) {
+                if (this.items[i].quality > minQuality) {
                     if (this.items[i].name != sulfuras) {
-                        this.items[i].quality--
+                        this.items[i].quality--;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality++
+                if (this.items[i].quality < maxQuality) {
+                    this.items[i].quality++;
                     if (this.items[i].name == backstagePasses) {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality++
+                        if (this.items[i].sellIn <= tenDaysTillConcert) {
+                            if (this.items[i].quality < maxQuality) {
+                                this.items[i].quality++;
                             }
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality++
+                        if (this.items[i].sellIn <= fiveDaysTillConcert) {
+                            if (this.items[i].quality < maxQuality) {
+                                this.items[i].quality++;
                             }
                         }
                     }
@@ -52,17 +56,17 @@ export class GildedRose {
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != brie) {
                     if (this.items[i].name != backstagePasses) {
-                        if (this.items[i].quality > 0) {
+                        if (this.items[i].quality > minQuality) {
                             if (this.items[i].name != sulfuras) {
-                                this.items[i].quality--
+                                this.items[i].quality--;
                             }
                         }
                     } else {
-                        this.items[i].quality = 0;
+                        this.items[i].quality = minQuality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality++
+                    if (this.items[i].quality < maxQuality) {
+                        this.items[i].quality++;
                     }
                 }
             }
